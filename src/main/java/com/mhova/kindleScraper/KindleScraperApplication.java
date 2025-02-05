9606c51db87c4a5fe2d3e938ef921660f9259708
Michael Ho <michael.chinghei.ho@gmail.com>
2025-02-05 09:51:44 -0800

    no-op: whitespace and unused imports

--- src/main/java/com/mhova/kindleScraper/KindleScraperApplication.java
+++ src/main/java/com/mhova/kindleScraper/KindleScraperApplication.java
@@ -6,24 +6,23 @@ import io.dropwizard.core.setup.Environment;
 
 public class KindleScraperApplication extends Application<KindleScraperConfiguration> {
 
-    public static void main(final String[] args) throws Exception {
-        new KindleScraperApplication().run(args);
-    }
+	public static void main(final String[] args) throws Exception {
+		new KindleScraperApplication().run(args);
+	}
 
-    @Override
-    public String getName() {
-        return "KindleScraper";
-    }
+	@Override
+	public String getName() {
+		return "KindleScraper";
+	}
 
-    @Override
-    public void initialize(final Bootstrap<KindleScraperConfiguration> bootstrap) {
-        // TODO: application initialization
-    }
+	@Override
+	public void initialize(final Bootstrap<KindleScraperConfiguration> bootstrap) {
+		// TODO: application initialization
+	}
 
-    @Override
-    public void run(final KindleScraperConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
-    }
+	@Override
+	public void run(final KindleScraperConfiguration configuration, final Environment environment) {
+		// TODO: implement application
+	}
 
 }
--- src/main/java/com/mhova/kindleScraper/KindleScraperConfiguration.java
+++ src/main/java/com/mhova/kindleScraper/KindleScraperConfiguration.java
@@ -1,10 +1,7 @@
 package com.mhova.kindleScraper;
 
 import io.dropwizard.core.Configuration;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import jakarta.validation.constraints.*;
 
 public class KindleScraperConfiguration extends Configuration {
-    // TODO: implement service configuration
+	// TODO: implement service configuration
 }
