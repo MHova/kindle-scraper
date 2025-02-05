68051f504d344bfed6be4ae92c27478ef52a2959
Michael Ho <michael.chinghei.ho@gmail.com>
2025-02-05 10:27:52 -0800

    Add job config fields

--- src/main/java/com/mhova/kindleScraper/KindleScraperConfiguration.java
+++ src/main/java/com/mhova/kindleScraper/KindleScraperConfiguration.java
@@ -1,7 +1,20 @@
 package com.mhova.kindleScraper;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.dropwizard.jobs.JobConfiguration;
 
 public class KindleScraperConfiguration extends JobConfiguration {
-	// TODO: implement service configuration
+	@JsonProperty("jobs")
+	private Map<String , String> jobs;
+
+	public Map<String, String> getJobs() {
+	    return jobs;
+	}
+
+	public void setJobs(Map<String, String> jobs) {
+	    this.jobs = jobs;
+	}
 }
