1278aed3c659612972536f31a55f0cc8556cd5f5
Michael Ho <michael.chinghei.ho@gmail.com>
2025-02-05 11:55:04 -0800

    Add jsoup dependency and parse Kindle price from static html

--- src/main/java/com/mhova/kindleScraper/jobs/ScrapeJob.java
+++ src/main/java/com/mhova/kindleScraper/jobs/ScrapeJob.java
@@ -1,17 +1,38 @@
 package com.mhova.kindleScraper.jobs;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.dropwizard.jobs.Job;
 import io.dropwizard.jobs.annotations.Every;
 
 @Every
 public class ScrapeJob extends Job {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapeJob.class);
 
 	@Override
-	public void doJob(JobExecutionContext context) throws JobExecutionException {
-		System.out.println("hello world");
+	public void doJob(final JobExecutionContext context) throws JobExecutionException {
+		final File html = new File("src/main/resources/kindle.htm");
+
+		try {
+			final Document document = Jsoup.parse(html);
+			final Element priceDisplayDiv = document.selectFirst("div[data-feature-name='corePriceDisplay_desktop']");
+			final String wholeDollarAndDecimalPoint = priceDisplayDiv.selectFirst("span.a-price-whole").text();
+			final String cents = priceDisplayDiv.selectFirst("span.a-price-fraction").text();
+
+			final Double price = Double.parseDouble(wholeDollarAndDecimalPoint + cents);
+			LOGGER.info("Kindle price is now " + price);
+		} catch (final IOException e) {
+			LOGGER.error(e.getMessage());
+		}
 	}
 
 }
