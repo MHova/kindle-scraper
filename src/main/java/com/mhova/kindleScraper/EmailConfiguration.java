e00438d206dc43fb1b6f578507ab931db55ed6bb
Michael Ho <michael.chinghei.ho@gmail.com>
2025-02-07 10:08:45 -0800

    no-op: add comment...
    ...about a thing that's probably not worth doing for this project

--- src/main/java/com/mhova/kindleScraper/EmailConfiguration.java
+++ src/main/java/com/mhova/kindleScraper/EmailConfiguration.java
@@ -1,5 +1,6 @@
 package com.mhova.kindleScraper;
 
+// TODO: Add a custom Jackson InternetAddress deserializer so that the recipient field can be type InternetAddress and recipient config validation will happen at startup
 public record EmailConfiguration(String user, String password, String recipient, String smtpHost, int smtpPort)
 		implements NotificationConfiguration {
 }
