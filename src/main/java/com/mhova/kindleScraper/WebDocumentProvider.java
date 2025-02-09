5cfea1ae1e9a38d1bd3cdc88d21f7ad4491a12eb
Michael Ho <michael.chinghei.ho@gmail.com>
2025-02-09 15:01:38 -0800

    These annotations are unnecessary

--- src/main/java/com/mhova/kindleScraper/WebDocumentProvider.java
+++ src/main/java/com/mhova/kindleScraper/WebDocumentProvider.java
@@ -6,15 +6,11 @@ import java.net.URL;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public final class WebDocumentProvider implements DocumentProvider {
 	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36";
 	private final URL url;
 
-	@JsonCreator
-	public WebDocumentProvider(@JsonProperty("url") final URL url) {
+	public WebDocumentProvider(final URL url) {
 		super();
 		this.url = url;
 	}
